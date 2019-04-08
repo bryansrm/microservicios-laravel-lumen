9631c7abc88824cc7e6ada963ad115934c4f0acc
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-08 15:32:51 -0500

    create, update, delete author

--- AuthorsApi/app/Http/Controllers/AuthorController.php
+++ AuthorsApi/app/Http/Controllers/AuthorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Traits\ApiResponser;
 use App\Author;
 
@@ -38,7 +39,17 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response
      */
     public function store(Request $request){
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
 
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+
+        return $this->successResponse($author, Response::HTTP_CREATED);
     }
 
     /**
@@ -47,7 +58,9 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response
      */
     public function show($author){
+        $author = Author::findOrFail($author);
 
+        return $this->successResponse($author);
     }
 
     /**
@@ -57,6 +70,24 @@ class AuthorController extends Controller
      */
     public function update(Request $request, $author){
 
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::findOrFail($author);
+
+        $author->fill($request->all());
+
+        if($author->isClean()){
+            return $this->errorResponse('Al menos debe cambiar un valor para actualizar', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+        return $this->successResponse($author);
     }
 
     /**
@@ -65,6 +96,10 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response
      */
     public function destroy($author){
+        $author = Author::findOrFail($author);
 
+        $author-> delete();
+
+        return $this->successResponse($author);
     }
 }
