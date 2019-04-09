909c39e4082ad0fb637cd36a6575f49373bd804c
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-09 09:50:31 -0500

    create controllers authors and book in Gateway

---
+++ GatewayApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Show a list all authors.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index(){
+
+    }
+
+    /**
+     * Save an author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request){
+
+    }
+
+    /**
+     * Show a specific author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($author){
+
+    }
+
+    /**
+     * Update a specific author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author){
+
+    }
+
+    /**
+     * Delete a specific author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author){
+
+    }
+
+
+}
---
+++ GatewayApi/app/Http/Controllers/BookController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Show a list all authors.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index(){
+
+    }
+
+    /**
+     * Save an book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request){
+
+    }
+
+    /**
+     * Show a specific book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($book){
+
+    }
+
+    /**
+     * Update a specific book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book){
+
+    }
+
+    /**
+     * Delete a specific book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book){
+
+    }
+
+
+}
